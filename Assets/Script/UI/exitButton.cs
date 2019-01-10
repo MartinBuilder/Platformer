f7d7f964a96995f2160a00d563592d7a6987d33a
Martin Keesom <25179@ma-web.nl>
2019-01-10 10:24:55 +0100

    StartScherm
    Het hele Start scherm zit erin en werkt

---
+++ Assets/Script/UI/exitButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class exitButton : MonoBehaviour
+{
+
+    public Button ExitButton;
+
+    void Start()
+    {
+        Button Exit = ExitButton.GetComponent<Button>();
+        Exit.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Script/UI/sceneLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class sceneLoader : MonoBehaviour {
+
+    public Button PlayButton;
+
+    void Start()
+    {
+        Button Play = PlayButton.GetComponent<Button>();
+        Play.onClick.AddListener(TaskOnClick);
+    }
+
+    public void SceneLoader(int SceneIndex)
+    {
+        SceneManager.LoadScene(SceneIndex);
+    }
+
+    void TaskOnClick()
+    {
+        SceneLoader(1);
+    }
+}
