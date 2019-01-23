84d21e8adfc49c50fbeca519719aca8c2db7a529
Sjors van der Kleij <24689@ma-web.nl>
2019-01-23 11:18:00 +0100

    button/Elevator

---
+++ Assets/Scriptz/Raycast/buttonPress.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buttonPress : MonoBehaviour {
+    Renderer rend;
+    private float range = 2;
+    public bool IsPressed = false;
+
+    void Start ()
+    {
+        
+        rend = GameObject.FindWithTag("Button").GetComponent<Renderer>();
+	}
+	
+	
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { 
+            RaycastHit hit;
+        if (Physics.Raycast(transform.position,transform.forward, out hit, range ))
+            {
+                Debug.Log(hit.transform.name);
+                if (hit.collider.tag == "Button")
+                {
+                    Debug.Log("clicked");
+                    rend.material.color = Color.green;
+                    IsPressed = true;
+                }
+            }
+        }
+	}
+}
