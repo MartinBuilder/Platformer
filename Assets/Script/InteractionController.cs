e50c4a603056779bf6312cfd789fb32ea0135931
Martin Keesom <25179@ma-web.nl>
2019-02-01 15:14:43 +0100

    CleanUp 1.1

--- Assets/Script/InteractionController.cs
+++ Assets/Script/InteractionController.cs
@@ -23,16 +23,13 @@ public class InteractionController : MonoBehaviour
         {
             RaycastHit hit;
             Vector3 fwd = transform.TransformDirection(Vector3.forward);
-            if (Physics.Raycast(transform.position, fwd, out hit))
+            if (Physics.Raycast(transform.position, fwd, out hit, 5f))
             {  
 
                 switch (hit.collider.gameObject.tag) { 
                         case "PickUp": hold = hit.collider.gameObject;if (hold.GetComponent<Rigidbody>()) { hold.GetComponent<Rigidbody>().useGravity = false; } break;
                         case "UpDown": case "LeftRight": case "RightLeft": hit.transform.parent.GetComponent<PlatformMovementController>().auto = false; hit.transform.parent.GetComponent<PlatformMovementController>().tag = hit.collider.tag; break; 
                 }
-
-              
-                
             }
                     
         }
